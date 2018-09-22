12367c0529b151e22937bd851d8e8a4905278677
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-22 21:00:07 +0530

    chennai times

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Bangalore  is: ' + currentTime.toString(),
+        body: 'The time in Chennai  is: ' + currentTime.toString(),
     });
 };
