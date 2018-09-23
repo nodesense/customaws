605316a4e6780c6a6679c68e5b3e7e7bcfd2cf11
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-23 09:58:26 +0530

    with change

--- category.js
+++ category.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The category  is: ' + currentTime.toString(),
+        body: 'The category 2 is: ' + currentTime.toString(),
     });
 };
